df1b9741217c9527b8e0ed59696081c85d6e341b
JaneAmi <evgenia.amineva@gmail.com>
2024-07-18 17:03:44 +0300

    works, but result much worse

--- models/models.py
+++ models/models.py
@@ -2,32 +2,37 @@
 
 
 import xgboost as xgb
-from xgboost import XGBClassifier
-
-from xgboost import XGBClassifier
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from sklearn.metrics import classification_report,confusion_matrix, roc_curve, roc_auc_score, auc, accuracy_score
 
 import matplotlib.pyplot as plt 
 
 
-class XGBClassifierWrapper:
-    def __init__(self, verbose=False):
+import xgboost as xgb
+
+
+
+import xgboost as xgb
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_curve, auc
+import matplotlib.pyplot as plt
+
+class XGBClassifierWrapper(xgb.XGBClassifier):
+    def __init__(self, verbose=False, **kwargs):
         """
         Initialize the XGBClassifierWrapper with model parameters.
         """
-        self.params = {
-                    'n_estimators': 3000,
-                    'objective': 'binary:logistic',
-                    'learning_rate': 0.005,
-                    'subsample': 0.555,
-                    'colsample_bytree': 0.7,
-                    'min_child_weight': 3,
-                    'max_depth': 8,
-                    'n_jobs': -1
-                    }
-        
-        self.model = XGBClassifier(**self.params)
+        default_params = {
+            'n_estimators': 3000,
+            'objective': 'binary:logistic',
+            'learning_rate': 0.005,
+            'subsample': 0.555,
+            'colsample_bytree': 0.7,
+            'min_child_weight': 3,
+            'max_depth': 8,
+            'n_jobs': -1
+        }
+        default_params.update(kwargs)
+        super().__init__(**default_params)
         self.verbose = verbose
 
     def fit(self, X_train, y_train, X_test, y_test):
@@ -35,7 +40,7 @@ class XGBClassifierWrapper:
         Fit the XGBoost model with the given training and test data.
         """
         eval_set = [(X_train, y_train), (X_test, y_test)]
-        self.model.fit(
+        super().fit(
             X_train, y_train, eval_set=eval_set,
             eval_metric='auc', early_stopping_rounds=100, verbose=self.verbose
         )
@@ -44,11 +49,11 @@ class XGBClassifierWrapper:
         """
         Make predictions using the trained XGBoost model.
         """
-        if hasattr(self.model, 'best_iteration'):
-            iteration_range = (0, self.model.best_iteration + 1)
-            return self.model.predict(X_test, iteration_range=iteration_range)
+        if hasattr(self, 'best_iteration'):
+            iteration_range = (0, self.best_iteration + 1)
+            return super().predict(X_test, iteration_range=iteration_range)
         else:
-            return self.model.predict(X_test)
+            return super().predict(X_test)
 
     def evaluate(self, X_test, y_test):
         """
@@ -61,39 +66,40 @@ class XGBClassifierWrapper:
             print("Model Final Generalization Accuracy: %.6f" % accuracy_score(y_test, y_pred))
         
         # Assuming get_roc is defined to calculate and display the ROC curve
-        y_pred_proba = self.model.predict_proba(X_test)[:, 1]
+        y_pred_proba = self.predict_proba(X_test)[:, 1]
         self.get_roc(y_test, y_pred_proba)  # This function needs to be defined elsewhere
 
-    def get_roc (self, y_test, y_pred):
+    def get_roc (self, y_test, y_pred_proba):
         # Compute ROC curve and ROC area for each class
-        fpr = dict()
-        tpr = dict()
-        roc_auc = dict()
-        fpr, tpr, _ = roc_curve(y_test, y_pred)
+        fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
         roc_auc = auc(fpr, tpr)
-        #Plot of a ROC curve
+        # Plot of a ROC curve
         plt.figure()
         lw = 2
         plt.plot(fpr, tpr, color='darkorange',
-                label='ROC curve (area = %0.2f)' % roc_auc)
+                 label='ROC curve (area = %0.2f)' % roc_auc)
         plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.0])
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
-        plt.title('Receiver operating characteristic')
-        plt.legend(loc="upper left")
+        plt.title('Receiver Operating Characteristic')
+        plt.legend(loc="lower right")
         plt.show()
-        return
 
     def get_model(self):
         """
         Return the internal XGBoost model.
         """
-        return self.model
+        return self
     
-    def get_params(self):
-
-        return self.model.get_params()
+    def get_params(self, deep=True):
+        return super().get_params(deep)
+    
+    def __call__(self, X):
+        """
+        Make the model callable to be compatible with SHAP.
+        """
+        return self.predict_proba(X)
 
 
